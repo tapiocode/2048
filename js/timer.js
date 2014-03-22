9ab1e692b3f7ef7101c987bbb9bc1ae001ab58e5
vthyyryn <ville.hyyrynen@gmail.com>
2014-03-22 16:01:56 +0200

    removed 64 and 128, added reset best times

--- js/timer.js
+++ js/timer.js
@@ -1,36 +1,36 @@
 function Timer() {
-	this.time = null;
+  this.time = null;
 }
 
 Timer.getFormatted = function(timeMillisec) {
-	function pad(num) {
-		return (num < 10 ? '0' : '') + num;
-	}
-	var totalSec = parseInt(timeMillisec / 1000, 10);
-	return [
-		pad(parseInt(totalSec / 60, 10) % 60),
-		pad(totalSec % 60),
-		pad(parseInt(timeMillisec % 1000 / 10, 10)),
-	].join(':');
+  function pad(num) {
+    return (num < 10 ? '0' : '') + num;
+  }
+  var totalSec = parseInt(timeMillisec / 1000, 10);
+  return [
+    pad(parseInt(totalSec / 60, 10) % 60),
+    pad(totalSec % 60),
+    pad(parseInt(timeMillisec % 1000 / 10, 10)),
+  ].join(':');
 }
 
 Timer.prototype.start = function() {
-	if (this.time === null) {
-		this.time = this._getNow();
-	}
+  if (this.time === null) {
+    this.time = this._getNow();
+  }
 }
 
 Timer.prototype.reset = function() {
-	this.time = null;
+  this.time = null;
 }
 
 Timer.prototype.getTime = function() {
-	if (this.time === null) {
-		return 0;
-	}
-	return this._getNow() - this.time;
+  if (this.time === null) {
+    return 0;
+  }
+  return this._getNow() - this.time;
 }
 
 Timer.prototype._getNow = function() {
-	return (new Date()).valueOf();
+  return (new Date()).valueOf();
 }
--- js/timer_manager.js
+++ js/timer_manager.js
@@ -1,45 +1,45 @@
 function TimerManager() {
-	this.timer = new Timer;
-	this.timesBest = new TimesBest(LocalStorageManager);
-	this.intervalId = null;
-	this.timerClockElem = document.getElementById('timer-clock');
-	this.timesBest.init();
+  this.timer = new Timer;
+  this.timesBest = new TimesBest(LocalStorageManager);
+  this.intervalId = null;
+  this.timerClockElem = document.getElementById('timer-clock');
+  this.timesBest.init();
 }
 
 TimerManager.prototype.start = function() {
-	this.timer.start();
-	this.startDrawingTime();
+  this.timer.start();
+  this.startDrawingTime();
 }
 
 TimerManager.prototype.stop = function() {
-	if (this.intervalId) {
-		clearInterval(this.intervalId);
-	}
-	this.intervalId = null;
+  if (this.intervalId) {
+    clearInterval(this.intervalId);
+  }
+  this.intervalId = null;
 }
 
 TimerManager.prototype.reset = function() {
-	this.stop();
-	this.timer.reset();
-	this.timerClockElem.textContent = Timer.getFormatted(0);
+  this.stop();
+  this.timer.reset();
+  this.timerClockElem.textContent = Timer.getFormatted(0);
 }
 
 TimerManager.prototype.continue = function() {
-	this.startDrawingTime(true);
+  this.startDrawingTime(true);
 }
 
 TimerManager.prototype.tileMerged = function(tileValue) {
-	var time = this.timer.getTime();
-	this.timesBest.setBlockTime(tileValue, time);
+  var time = this.timer.getTime();
+  this.timesBest.setBlockTime(tileValue, time);
 }
 
 TimerManager.prototype.startDrawingTime = function(cont) {
-	var self = this;
-	if (this.intervalId === null || cont) {
-		this.intervalId = setInterval(function() {
-			self.timerClockElem.textContent = Timer.getFormatted(self.timer.getTime());
-		}, 50);
-	}
+  var self = this;
+  if (this.intervalId === null || cont) {
+    this.intervalId = setInterval(function() {
+      self.timerClockElem.textContent = Timer.getFormatted(self.timer.getTime());
+    }, 50);
+  }
 }
 
 
